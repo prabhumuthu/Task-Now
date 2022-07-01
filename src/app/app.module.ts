e68ecb8346593e53a98943114a45c8494e7d750f
karthickthinkcove <karthickveerappan92@gmail.com>
2022-07-01 15:30:39 +0530

    my-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisComponent } from './regis/regis.component';
+import { TaskComponent } from './task/task.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+
+
+
+
+const routes:Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'regis',component:RegisComponent},
+  {path:'task',component:TaskComponent},
+    
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisComponent,
+    TaskComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
